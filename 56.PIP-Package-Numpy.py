0565ca4f5000439fe0066e2639205cdc168bee0c
FaisalMuaris <faisal.muaris.fm@gmail.com>
2023-02-10 13:01:25 +0700

    modified

--- 56.PIP-Package-Numpy.py
+++ 56.PIP-Package-Numpy.py
@@ -3,8 +3,8 @@ import os
 
 os.system('cls')
 
-list_a = [1,2,3,4]
-vector_a = np.array([1,2,3,4])
+list_a = [1, 2, 3, 4]
+vector_a = np.array([1, 2, 3, 4])
 
 print(f"list_a = {list_a} ")
 # print(list_a**2) # gagal
@@ -12,14 +12,14 @@ print(f"vector_a = {vector_a} ")
 print(f"a pangkat 2 = {vector_a**2} ")
 print(f"a kali 3 = {vector_a*5} ")
 
-matrix_b = np.array([(1,2),(3,4)])
+matrix_b = np.array([(1, 2), (3, 4)])
 print(f"ini adalah matrix b = \n{matrix_b} ")
 print(f"matrix b pangkat 2 = \n{matrix_b**2} ")
 
-zeros_c = np.zeros((2,2))
+zeros_c = np.zeros((2, 2))
 print(f"zeros c = \n{zeros_c} ")
 
-ones_d = np.ones((2,2))
+ones_d = np.ones((2, 2))
 print(f"ones d = \n{ones_d} ")
 
 jumlah = matrix_b + matrix_b**2 + ones_d
